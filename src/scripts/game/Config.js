d182bbf04591b9b72a0a0219dd2d55c455eaf448
Ilina Karlova <ilinakarlova@Ilinas-MacBook-Air.local>
2023-04-17 22:12:25 +0300

    First commit

---
+++ src/scripts/game/Config.js
@@ -0,0 +1,21 @@
+import { Game } from "./Game";
+import { Tools } from "../system/Tools";
+
+export const Config = {
+    loader: Tools.massiveRequire(require["context"]('./../../sprites/', true, /\.(mp3|png|jpe?g)$/)),
+    scenes: {
+        "Game": Game,
+    },
+    board: {
+        rows: 8,
+        cols: 8
+    },
+    tilesColors: ['blue', 'green', 'orange', 'red', 'pink', 'yellow'],
+    combinationRules: [[
+        {col: 1, row: 0}, {col: 2, row: 0},
+    ],[
+        {col: 0, row: 1}, {col: 0, row: 2},
+    ]
+
+    ]
+};
